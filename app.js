86b1626a30a1423f8b1c8a41b080f5131ed78035
Sahith Kota <sahithkota@Sahiths-Air.Dlink>
2022-12-06 15:46:55 +0530

    a9 Mongo fa22

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ import cors from 'cors';
 import HelloController from './controllers/hello-controller.js';
 import UserController from './controllers/users/users-controller.js';
 import TuitsController from './controllers/tuits/tuits-controller.js';
+import mongoose from "mongoose";
 
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+ || 'mongodb://localhost:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(express.json());
 app.use(cors());
 HelloController(app);
 UserController(app);
 TuitsController(app);
-app.listen(process.env.PORT || 4001);
+app.listen(process.env.PORT || 4000);
